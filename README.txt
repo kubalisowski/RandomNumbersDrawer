3c0c16dbdb868da7dd1c21b520846146a7303af0
Kuba <kubalisowski94@gmail.com>
2019-07-02 23:04:48 +0200

    Version 2
    With config file
    No config data validation

---
+++ README.txt
@@ -0,0 +1,18 @@
+###################################
+###### Author: Kuba Lisowski ######
+##### kubalisowski4@gmail.com #####
+###################################
+
+Aplication's role is to draw a random number from config file.
+
+<-- config.txt -->
+line:
+	1st: minial number
+	2st: maximal number
+	3rd: Text for case if all numbers were chosen
+		-> number draw will be repeated in next application open
+	4th: Regular text to drawn number
+		-> number chosen from range between minimal and maximal values
+
+<-- numbers.txt -->
+store already drawn numbers
--- new.py
+++ new.py
@@ -1,59 +1,84 @@
+import sys
 import random
 
 class Variables:
-    file = 'numbers.txt'
+    numbers = 'numbers.txt'
+    config = 'config.txt'
+    confDict = ('minimal', 'maximal', 'counterStrike', 'regularText')
+    error = 'Can\'t load the configuration file.\nPlease check values within config.txt'
+
+class Config:
+    def __init__(self, config, confDict, error):
+        self.config = config
+        self.confDict = confDict
+        self.error = error
+
+    def readConfig(self):
+        confList = list()
+        with open(self.config) as c:
+            for line in c.readlines():
+                confList.append(line.strip())
+        config = dict(zip(self.confDict, confList))
+        return config
 
 class Read:
-    def __init__(self, file):
-        self.file = file
+    def __init__(self, numbers):
+        self.numbers = numbers
 
-    def readFile(self):
+    def readnumbers(self):
         numsRead = list()
-        with open(self.file) as f:
+        with open(self.numbers) as f:
             for line in f.readlines():
                 numsRead.append(line.rstrip())
             f.close()
         return numsRead
 
 class Draw:
-    def __init__(self, numsRead, file):
+    def __init__(self, numsRead, numbers, config):
         self.numsRead = numsRead
-        self.file = file
+        self.numbers = numbers
+        self.config = config
 
     def drawNum(self):
-        if len(self.numsRead) is 150:
-            with open(self.file, 'w') as f:
+        minimal = int(self.config['minimal'])
+        maximal = int(self.config['maximal'])
+        numsFull = list()
+
+        for n in range(minimal, maximal + 1):
+            numsFull.append(str(n))
+
+        if len(self.numsRead) is len(numsFull):
+            with open(self.numbers, 'w') as f:
                 f.close()
-            return print('Wszystkie psalmy zostały wylosowane!\nZerowanie licznika')
+            return print(self.config['counterStrike'])
 
         if len(self.numsRead) is 0:
-            firstNum = random.choice(range(1,150))
-            with open(self.file, 'w') as f:
+            firstNum = random.choice(range(minimal, maximal + 1))
+            with open(self.numbers, 'w') as f:
                 f.write(str(firstNum))
             f.close()
-            return print('FIRSTWylosowany psalm: ' + str(firstNum) + '\n')
+            return print(self.config['regularText'] + ' ' + str(firstNum))
         else:
-            numsFull = list()
-            for n in range(1, 151):
-                numsFull.append(str(n))
-
             numsDifference = list(set(numsFull) - set(self.numsRead))
             randNum = random.choice(numsDifference)
 
             self.numsRead.append(randNum)
 
-            with open(self.file, 'w') as f:
+            with open(self.numbers, 'w') as f:
                 for n in self.numsRead:
                     f.write('%s\n' % n)
                 f.close()
 
-            return print('Wylosowany psalm: ' + str(randNum) + '\n')
+            return print(self.config['regularText'] + ' ' + str(randNum))
 
 
 Var = Variables()
 
-ReadObj = Read(Var.file)
-readVar = ReadObj.readFile()
+ReadObj = Read(Var.numbers)
+readVar = ReadObj.readnumbers()
 
-DrawObj = Draw(readVar, Var.file)
+ConfigObj = Config(Var.config, Var.confDict, Var.error)
+confVar = ConfigObj.readConfig()
+
+DrawObj = Draw(readVar, Var.numbers, confVar)
 drawVar = DrawObj.drawNum()
